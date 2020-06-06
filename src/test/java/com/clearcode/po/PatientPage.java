8fb6921d772250f6eed43eeaec6828a18341a00a
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 18:25:11 +0530

    B

--- src/test/java/com/clearcode/po/PatientPage.java
+++ src/test/java/com/clearcode/po/PatientPage.java
@@ -6,4 +6,6 @@ public class PatientPage {
 	public static By title_select = By.id("form_title");
 	public static By fname_ip = By.id("form_fname");
 	public static By lname_ip = By.id("form_lname");
+	//Hi4
+	//Hi5
 }
