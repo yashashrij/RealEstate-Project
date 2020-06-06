1481e37144083e1ee89fa774d311604ca5f31708
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 18:02:41 +0530

    Test

--- src/test/java/com/clearcode/po/HomePage.java
+++ src/test/java/com/clearcode/po/HomePage.java
@@ -3,7 +3,6 @@ package com.clearcode.po;
 import org.openqa.selenium.By;
 
 public class HomePage {
-	//new line
 	public static By username_ip = By.id("authUse");
 	public static By pwd_ip = By.id("clearPass");
 	public static By login_btn = By.className("fa-sign-in");
