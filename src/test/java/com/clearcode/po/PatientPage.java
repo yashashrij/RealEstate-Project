f651e73b2ce2b09e033fd3a4eb7f0abab111ffb8
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 18:23:33 +0530

    A

--- src/test/java/com/clearcode/po/PatientPage.java
+++ src/test/java/com/clearcode/po/PatientPage.java
@@ -3,12 +3,6 @@ package com.clearcode.po;
 import org.openqa.selenium.By;
 
 public class PatientPage {
-<<<<<<< HEAD
-	//new
-=======
-	
-	
->>>>>>> branch 'master' of https://github.com/natramu93/FrameworkCCCS.git
 	public static By title_select = By.id("form_title");
 	public static By fname_ip = By.id("form_fname");
 	public static By lname_ip = By.id("form_lname");
