2014fd8f3131dacec132ba09267da147c1b1809e
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 17:57:50 +0530

    A

--- src/test/java/com/clearcode/po/MenuBar.java
+++ src/test/java/com/clearcode/po/MenuBar.java
@@ -3,6 +3,7 @@ package com.clearcode.po;
 import org.openqa.selenium.By;
 
 public class MenuBar {
+	//new line
 	public static By patient_menu = By.xpath("//div[text()='Patient/Client']");
 	public static By newPatient_menu = By.xpath("//div[text()='New/Search']");
 }
