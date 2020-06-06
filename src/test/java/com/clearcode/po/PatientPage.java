a1b362509feaa7b9b86c14ab3fb65ee974f4f526
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 18:08:23 +0530

    E

--- src/test/java/com/clearcode/po/PatientPage.java
+++ src/test/java/com/clearcode/po/PatientPage.java
@@ -3,9 +3,8 @@ package com.clearcode.po;
 import org.openqa.selenium.By;
 
 public class PatientPage {
-	//new line from github
+	//new line
 	public static By title_select = By.id("form_title");
 	public static By fname_ip = By.id("form_fname");
 	public static By lname_ip = By.id("form_lname");
-	
 }
