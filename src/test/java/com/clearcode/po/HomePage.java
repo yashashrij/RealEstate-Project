f4829f47dcf738cdc8686a58eb3524b0141c515c
Natarajan Ramanathan <natarajan.ramanathan93@gmail.com>
2020-06-06 18:22:38 +0530

    F

--- src/test/java/com/clearcode/po/HomePage.java
+++ src/test/java/com/clearcode/po/HomePage.java
@@ -6,4 +6,6 @@ public class HomePage {
 	public static By username_ip = By.id("authUse");
 	public static By pwd_ip = By.id("clearPass");
 	public static By login_btn = By.className("fa-sign-in");
+	
+	
 }
